61cde33c11cfcd170afca9b771e26a4c7f3ecc28
mon5tery <ahmed.alrasheed88@gmail.com>
2019-10-03 02:15:44 +0300

    import issues

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+const Loading = () => {
+  return (
+    <div className="spinner mx-auto text-center">
+      <FontAwesomeIcon icon={faSpinner} spin size="4x" />
+    </div>
+  );
+};
+
+export default Loading;
---
+++ src/stores/authStore.js
@@ -0,0 +1,70 @@
+// import { decorate, observable, computed } from "mobx";
+// import axios from "axios";
+// import jwt_decode from "jwt-decode";
+// import data from "./data.js";
+
+// const instance = axios.create({
+//   baseURL: "Faker.js"
+// });
+
+// class AuthStore {
+//   user = null;
+//   setUser = token => {
+//     if (token) {
+//       axios.defaults.headers.common.Authorization = `JWT ${token}`;
+//       const decodedUser = jwt_decode(token);
+//       this.user = decodedUser;
+//       localStorage.setItem("myToken", token);
+//     } else {
+//       delete axios.defaults.headers.common.Authorization;
+//       localStorage.removeItem("myToken");
+//       this.user = null;
+//     }
+//   };
+
+//   signup = async (userData, history) => {
+//     try {
+//       await instance.post("/signup/", userData);
+//       await history.replace("/");
+//     } catch (err) {
+//       console.error(err.response.data);
+//     }
+//   };
+
+//   login = async (userData, history) => {
+//     try {
+//       const res = await instance.post("/login/", userData);
+//       const user = res.data;
+//       this.setUser(user.token);
+//       history.replace("/");
+//     } catch (err) {
+//       console.error(err.response.data);
+//     }
+//   };
+
+//   logout = () => {
+//     this.setUser();
+//   };
+
+//   checkForToken = () => {
+//     const token = localStorage.getItem("myToken");
+//     if (token) {
+//       const currentTime = Date.now() / 1000;
+//       const user = jwt_decode(token);
+//       if (user.exp >= currentTime) {
+//         this.setUser(token);
+//       } else {
+//         this.logout();
+//       }
+//     }
+//   };
+// }
+
+// decorate(AuthStore, {
+//   user: observable
+// });
+
+// const authStore = new AuthStore();
+// authStore.checkForToken();
+
+// export default authStore;
