a7fee91d751d28cb4ebdbfe3ce0a78a9a3c1124f
mon5tery <ahmed.alrasheed88@gmail.com>
2019-10-04 23:23:45 +0300

    merging with Mulla

--- src/forms/loginForm.js
+++ src/forms/loginForm.js
@@ -1,75 +1,113 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import authStore from "../stores/authStore";
-import { observer } from "mobx-react";
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-// Styling
-import "./LoginForm.css";
-import { Container } from "semantic-ui-react";
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100vh"
+  },
+  image: {
+    backgroundImage:
+      "url(http://getwallpapers.com/wallpaper/full/a/5/d/544750.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
 
-class LoginForm extends Component {
-  state = {
-    username: "",
-    password: ""
-  };
-
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  componentDidMount() {
-    if (authStore.user) {
-      this.props.history.push("/");
-    }
+    marginbottom: "auto",
+    marginleft: "auto",
+    marginright: "auto"
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
   }
-  handleSubmit = event => {
-    event.preventDefault();
-    authStore.login(this.state, this.props.history);
-  };
+}));
 
-  render() {
-    const { username, password } = this.state;
-    return (
-      <Container className="container">
-        <div className="card">
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <div>
-                <label>Username</label>
-                <input
-                  className="input-group-prepend span"
-                  type="text"
-                  id="username"
-                  value={username}
-                  name="username"
-                  placeholder="Username"
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div>
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  value={password}
-                  name="password"
-                  placeholder="Password"
-                  onChange={this.handleChange}
-                />
-              </div>
+export default function SignInSide() {
+  const classes = useStyles();
 
-              <button type="submit" className="login_btn login_btn:hover">
-                LoginForm
-              </button>
-              <Link to="/signup/" className="links">
-                Signup for an account
-              </Link>
-            </form>
-          </div>
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            {/* <LockOutlinedIcon />? */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/signup/" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-      </Container>
-    );
-  }
+      </Grid>
+    </Grid>
+  );
 }
-
-export default observer(LoginForm);
