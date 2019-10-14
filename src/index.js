0950000b5ed7c91de7496ae01a9906d39222bd59
MMulla <zore.mulla@gmail.com>
2019-10-14 17:05:54 +0300

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 // import './index.css';
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
