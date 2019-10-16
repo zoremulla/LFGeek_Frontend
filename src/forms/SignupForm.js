edeccab1f842255f21573ebd9f2e3909076f1cdc
mon5tery <ahmed.alrasheed88@gmail.com>
2019-10-17 01:19:05 +0300

    the submit expects a list

--- src/forms/SignupForm.js
+++ src/forms/SignupForm.js
@@ -29,7 +29,7 @@ class Signup extends Component {
   };
   componentDidMount() {
     if (authStore.user) {
-      this.props.history.push("/");
+      this.props.history.push("profile/");
     }
   }
 
---
+++ src/forms/games.js
@@ -0,0 +1,9 @@
+const games = [
+  ["WOW", "WOW"],
+  ["Dota 2", "Dota 2"],
+  ["Fortnite", "Fortnite"],
+  ["Overwatch", "Overwatch"],
+  ["Heroes of the Storm", "Heroes of the Storm"]
+];
+
+export default games;
